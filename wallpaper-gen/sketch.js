14d34d1f73400137f71f7a654f0a3ece5bae0512
mvaladas <mvaladas@users.noreply.github.com>
2022-08-14 14:24:22 +0000

    deploy: mvaladas/hugo-cv@e07d70f0bf520af47e1568b2c0b2676a1b28493e

---
+++ wallpaper-gen/sketch.js
@@ -0,0 +1,71 @@
+var mainColor;
+var offInc = 0.005
+var ranges = 3;
+var skySize;
+var landSize;
+var output;
+var canvas;
+var scaleOutput = 1;
+
+function setup() {
+  createCanvas(270, 480);
+  colorMode(HSB, 255);
+  mainColor = color(random(255), random(200), random(100, 200));
+  skySize = height / 6;
+  landSize = height - skySize;
+  output = createGraphics(270,480);
+  output.colorMode(HSB, 255);
+
+  // button = createButton('Refresh');
+  // button.mousePressed(Refresh);
+  button = createButton('Download High-res');
+  button.mousePressed(DL);
+}
+
+function draw() {
+   // Clear Canvas
+   background(255);
+   output.clear();
+  output.push();
+  output.scale(scaleOutput);
+  output.background(mainColor);
+  output.noStroke()
+  let sat = output.saturation(mainColor);
+  let bright = output.brightness(mainColor);
+  var xoff = 0;
+  for (let i = 0; i < ranges; i++) {
+    bright *= 1.1;
+    sat *= 0.8;
+    output.fill(hue(mainColor), sat, bright);
+    output.beginShape();
+    output.vertex(width, height);
+    output.vertex(0, height);
+    for (let x = 0; x < width; x++) {
+      // let y = map(noise(xoff), 0, 1, 
+      // (((height-skySize) / ranges) * (i)) + skySize,
+      // (((height-skySize) / ranges) * (i + 1)) + skySize)
+      let y = map(noise(xoff), 0, 1, skySize+(landSize / ranges)*i, height);
+      output.vertex(x, y);
+      // xoff += offInc * (ranges - i) * 2;
+      xoff += offInc;
+    }
+    output.endShape(CLOSE);
+    xoff += 10;
+  }
+  output.pop();
+  image(output, 0, 0);
+  noLoop();
+}
+
+function DL() {
+  // HighRes Export
+  scaleOutput = 8;
+  output = createGraphics(scaleOutput * 270, scaleOutput * 480);
+  draw();
+  save(output, "filename", 'png');
+
+  // Reset Default
+  scaleOutput = 1;
+  output = createGraphics(270,480);
+  draw();
+}
